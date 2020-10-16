9a6ceeab76975b58fc5c8734abb8b59511685ecd
Thai Do <thai.hl.do@gmail.com>
2020-10-15 17:56:58 -0700

    Created feature for User Authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+# A controller for the users inherits application controller
+class UsersController < ApplicationController
+
+  def new
+  end
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/', notice: 'Account created successfully'
+    else
+      redirect_to '/signup'
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end 
