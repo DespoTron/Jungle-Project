18da7216a2a72fd270b985972b6e8c707292a52a
Thai Do <thai.hl.do@gmail.com>
2020-10-19 16:48:01 -0700

    Created tests/validations for name, price, quantity and category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,5 +10,37 @@ RSpec.describe Product, type: :model do
       new_product.save
       expect(new_product).to be_valid
     end
+
+    it 'should not validate a product without a name' do
+      new_category = Category.new(name: "Hats")
+      new_product = Product.new(category: new_category, name: nil, price_cents: 5000, quantity: 50)
+      new_product.save
+      expect(new_product).to_not be_valid
+      expect(new_product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'should not validate a product without a price' do
+      new_category = Category.new(name: "Hats")
+      new_product = Product.new(category: new_category, name: "cloche", price_cents: nil, quantity: 50)
+      new_product.save
+      expect(new_product).to_not be_valid
+      expect(new_product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'should not validate a product without quanity' do
+      new_category = Category.new(name: "Hats")
+      new_product = Product.new(category: new_category, name: "cloche", price_cents: 5000, quantity: nil)
+      new_product.save
+      expect(new_product).to_not be_valid
+      expect(new_product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'should not validate a product without a category' do
+      new_product = Product.new(category: nil, name: "cloche", price_cents: 5000, quantity: 50)
+      new_product.save
+      expect(new_product).to_not be_valid
+      expect(new_product.errors.full_messages).to include("Category can't be blank")
+    end
+    
   end
 end
