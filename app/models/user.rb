2b748848fcb29eb4c0bf3c6d6c57f0eddb098a90
Thai Do <thai.hl.do@gmail.com>
2020-10-19 17:22:35 -0700

    Added validation specs and password minimum length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  
+  validates :password, presence: true
+  validates :password_confirmation, presence: true  
+  validates :password, length: { minimum: 6 }
+  validates_uniqueness_of :email :case_senstive => false
+  validates :first_name, :last_name, :email, presence true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  
+  describe 'Validations for Users model' do
+    
 
 end
