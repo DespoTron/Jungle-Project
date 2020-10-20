7eba329f2d2ef940cd8cd454ce3321a05f362f82
Thai Do <thai.hl.do@gmail.com>
2020-10-19 19:03:25 -0700

    Second test passes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,13 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
 
+    it "should validate the password, but be invalid when password_confirmation does not match" do
+      user = User.new(first_name: 'Zeus', last_name: 'Smith', email: 'test@test.com', password: 'C^t+6C', password_confirmation: 'C^t+6CA')
+      user.save
+      expect(user).to_not be_valid
+    end
+
     
 
   end
-
 end
