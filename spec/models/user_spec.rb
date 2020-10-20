e5d1d9b66d88d2274b1eca7341c6767792d49556
Thai Do <thai.hl.do@gmail.com>
2020-10-19 19:01:15 -0700

    Fixed migrations/schema/user controller and got first test to work

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe 'Validations' do
+  describe 'Validations for Users Model' do
     # validation examples here
+    it "is valid with password and password_confirmation matching" do
+      user = User.new(first_name: 'Zeus', last_name: 'Smith', email: 'test@test.com', password: 'C^t+6C', password_confirmation: 'C^t+6C')
+      user.save
+      expect(user).to be_valid
+    end
+
+    
+
   end
 
-  describe '.authenticate_with_credentials' do
-    # examples for this class method here
-  end
 end
