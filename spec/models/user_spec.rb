b507280fc963b212f7897d32dabda23d9d93d033
Thai Do <thai.hl.do@gmail.com>
2020-10-19 17:48:12 -0700

    Added new authentication class method)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe 'Validations for Users model' do
-    
+  describe 'Validations' do
+    # validation examples here
+  end
 
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
 end
